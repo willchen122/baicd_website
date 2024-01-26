f9fb5babd4b9fc17e28c23af19a164fbc1c43114
William Chen <williamchen@Williams-MacBook-Pro-2.local>
2024-01-25 20:36:20 -0800

    added text for 'about' page

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,9 +11,23 @@ const About = () => {
             }}
         >
             <h1>
-                GeeksforGeeks is a Computer Science portal
-                for geeks.
+                ABOUT
             </h1>
+
+            <p>
+            BAICD is a collective of independent Chinese dancers, choreographers, 
+            and allied artists who use traditional arts to tell new Bay Area and 
+            global stories in our own voices. While we were created to provide a 
+            gender-inclusive home for 22-50 year-old Chinese dancers, we are open 
+            to anyone who shares our passion for creating new work in traditional 
+            arts and original interpretations of derived pieces, regardless of age, 
+            national origin, gender, or other dance team/studio affiliations. Our 
+            goal is not to replicate the traditional studio model, but to focus on 
+            original choreographies and solos that better fit with working adults’ 
+            schedules while democratizing individual dancers’ artistic voice. We hope 
+            to connect traditional audiences with local issues, and use our public 
+            engagement to introduce new communities to the diversity of Chinese dance. 
+            </p>
         </div>
     );
 };
