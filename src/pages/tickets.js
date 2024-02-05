31403de551a16c5d111451c37697b910c377eb3a
Joy Hwang <joyhwangbvt2023@gmail.com>
2024-02-04 22:24:06 -0800

    Modified media & tickets page and link styling

--- src/pages/tickets.js
+++ src/pages/tickets.js
@@ -33,11 +33,11 @@ const Ticket = () => {
                 <img src={BAICD_BannerImage}></img>
             </div>
 
-            <div>                
+            <div className="ticketTexts">                
                 <strong>閩橋 Min Bridges: stories from the Malay World, Taiwan, and mainland China</strong> is our first produced show, on May 4th, 2024 at 7:30 pm at Theatre of Yugen in San Francisco. Stay tuned for the official ticket site. In the meantime, you can secure early access to the ticketing website by donating to our company below  and keep in touch by signing up for our mailing list on the Contact/Booking tab.  
                 <br></br>
                 <br></br>
-                <strong>Donations</strong> Donors like you are a valuable part of our community here at BAICD. We’re all volunteers, but we aspire to pay our members and collaborators equitable market rates, We can’t do that without your support.  You help us secure the innovative, equitable, and democratic traditional arts we all believe in. Thanks to our fiscal sponsor Shawl Anderson Dance Center (SADC), all donations to BAICD are tax-deductible. Please donate through SADC’s website <a className="donationLink" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target="_blank">here</a>.
+                <strong>Donations</strong> Donors like you are a valuable part of our community here at BAICD. We’re all volunteers, but we aspire to pay our members and collaborators equitable market rates, We can’t do that without your support.  You help us secure the innovative, equitable, and democratic traditional arts we all believe in. Thanks to our fiscal sponsor Shawl Anderson Dance Center (SADC), all donations to BAICD are tax-deductible. Please donate through SADC’s website <a className="allLinks" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" target="_blank">here</a>.
             </div>
         </div>
     );
