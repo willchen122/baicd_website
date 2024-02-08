cbf2af39ba9d0a544cda1d5c0db98d70163c5d76
Joy Hwang <joyhwangbvt2023@gmail.com>
2024-02-07 21:47:55 -0800

    Added booking form, modified links, formatted text

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -34,48 +34,54 @@ const Contact = () => {
             <h3>
                 CONTACT/BOOKING
             </h3>
-            <p>
-            Please fill out our form below to book us for public or private performances or panels, collaborations, business proposals, press inquiries, &c. We love being a part of the community, and we look forward from hearing from you! You can also use this form to sign up for our mailing list! 
-            </p>
-            <div className="booking-form">
-            <form>
-                <div className="leftColContact">
-                <label for="name">Name</label><br></br>
-                <input type="text" id="name" name="name" /><br></br>
-                <label for="email">Email address</label><br></br>
-                <input type="text" id="email" name="email" /><br></br>
+            <div className="bookingText">
+                <p className="booking-disclaimer">
+                Please fill out our form below to book us for public or private performances or panels, collaborations, business proposals, press inquiries, &c. We love being a part of the community, and we look forward from hearing from you! You can also use this form to sign up for our mailing list! 
+                </p>
+                <div className="booking-form">
+                <form>
+                    <div className="leftColContact">
+                    <label for="name">Name</label><br></br>
+                    <input type="text" id="name" name="name" /><br></br>
+                    <label for="email">Email address</label><br></br>
+                    <input type="text" id="email" name="email" /><br></br>
+                    </div>
+
+                    <div className="rightColContact">
+                    <label for="phoneNum">Phone number</label><br></br>
+                    <input type="text" id="phoneNum" name="phoneNum" /><br></br>
+                    <label for="inquiryChoice">Inquiry type</label><br></br>
+
+                    <input list="inquiry-choice" id="inquiryChoice" name="inquiryChoice" />
+
+                    <datalist id="inquiry-choice">
+                        <option value="Book"></option>
+                        <option value="General"></option>
+                        <option value="Get in contact"></option>
+                        <option value="Customer support"></option>
+                        <option value="Buy tickets"></option>
+                    </datalist>
+                    <br></br>
+                    </div>
+
+                    <div className="bottomContact">
+                    <div className="messagesBox">
+                        <label for="messages">Messages</label><br></br>
+                        <input type="text" id="messages" name="messages" /><br></br>
+                    </div>
+                    <div className="signUpBox">
+                        <input type="checkbox" id="sign-up" name="sign-up" />
+                        <label for="sign-up">Sign up for our mailing list</label><br></br>
+                    </div>
+                    </div>
+                </form>
+                <div className="bottomIcons">
+                    {/* still gotta do this */}
                 </div>
-
-                <div className="rightColContact">
-                <label for="phoneNum">Phone number</label><br></br>
-                <input type="text" id="phoneNum" name="phoneNum" /><br></br>
-                <label for="inquiryChoice">Inquiry type</label><br></br>
-
-                <input list="inquiry-choice" id="inquiryChoice" name="inquiryChoice" />
-
-                <datalist id="inquiry-choice">
-                    <option value="Book"></option>
-                    <option value="General"></option>
-                    <option value="Get in contact"></option>
-                    <option value="Customer support"></option>
-                    <option value="Buy tickets"></option>
-                </datalist>
+                </div>
                 <br></br>
-                </div>
-
-                <div className="bottomContact">
-                <label for="messages">Messages</label><br></br>
-                <input type="text" id="messages" name="messages" /><br></br>
-                <label for="sign-up">Sign up for our mailing list</label><br></br>
-                <input type="checkbox" id="sign-up" name="sign-up" /><br></br>
-                </div>
-            </form>
-            <div className="bottomIcons">
-                
+                <p className="booking-disclaimer">Photography by Zoe Moore, Isabelle Hodge, Nathaniel Goh, and others and logo designs by Tiffany Lin Art <a className="allLinks" href="https://www.instagram.com/tiffanyyilinart/" target="blank">(https://www.instagram.com/tiffanyyilinart/)</a>. Bay Area Independent Chinese Dancers is a fiscally sponsored project of Shawl Anderson Dance Center. Website design by Shawn Lee, @studiocreator and @hopdesignstudio on Canva and implemented by Nala Chen. All rights reserved.</p>
             </div>
-            </div>
-            <br></br>
-            <span>Photography by Zoe Moore, Isabelle Hodge, Nathaniel Goh, and others and logo designs by Tiffany Lin Art (https://www.instagram.com/tiffanyyilinart/). Bay Area Independent Chinese Dancers is a fiscally sponsored project of Shawl Anderson Dance Center. Website design by Shawn Lee, @studiocreator and @hopdesignstudio on Canva and implemented by Nala Chen. All rights reserved.</span>
         </div>
     );
 };
--- src/pages/media.js
+++ src/pages/media.js
@@ -37,8 +37,8 @@ const Media = () => {
                 <br></br>
 
                 <div className="linkReview"><p className="mediaSpan">"<a className="allLinks" href="https://www.youtube.com/watch?v=djV11Xbc914&ab_channel=a-ha" target="blank">Dance Mission Theater Presents HARVEST Choreographers Showcase</a>" Jenn Norris Dance Reviews 9 Oct 2023</p></div>
+                <div className="reviewTexts">“Two solos drawn from world dance forms shine brightly on this evening. The first, Keluarga Laut (Sea Families) is choreographed and performed by Shawn Lee to a light-hearted song sung by a Chinese high school choir. The movement, with its repeating gestures to the sky and the sea, has the flavor of classical Chinese dance. Lee’s focus is a woven Chinese fishing hat with a domed top and wide round brim. Lee wears a calf length skirt, which highlights the frequent backward foot flicks that create the dance’s accents. Smiling broadly with joyous energy Lee windmills and then spins dervish-like hat in hand. I imagine if this dance is traditional, it is danced to invite or celebrate a successful fishing outing.”</div>
             </div>
-            <div className="reviewTexts">“Two solos drawn from world dance forms shine brightly on this evening. The first, Keluarga Laut (Sea Families) is choreographed and performed by Shawn Lee to a light-hearted song sung by a Chinese high school choir. The movement, with its repeating gestures to the sky and the sea, has the flavor of classical Chinese dance. Lee’s focus is a woven Chinese fishing hat with a domed top and wide round brim. Lee wears a calf length skirt, which highlights the frequent backward foot flicks that create the dance’s accents. Smiling broadly with joyous energy Lee windmills and then spins dervish-like hat in hand. I imagine if this dance is traditional, it is danced to invite or celebrate a successful fishing outing.”</div>
         </div>
     );
 };
