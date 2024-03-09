278dbe3a961ec87651be4cc2ab2edfec9bc5b106
Yiyu Chen <yiyuchen@Yiyus-MacBook-Pro.local>
2024-03-09 00:30:05 -0800

    fix background color

--- src/components/Navbar/navbarElements.js
+++ src/components/Navbar/navbarElements.js
@@ -3,7 +3,7 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
  
 export const Nav = styled.nav`
-    background: white;
+    background: #f8f7f6;
     height: 125px;   
     display: flex;
     justify-content: space-between;
