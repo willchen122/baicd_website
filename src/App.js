2e4f2323f2ba2bf7f2fbb43d8e073cdb8bdbde8a
Yiyu Chen <yiyuchen@Yiyus-MacBook-Pro.local>
2024-03-09 00:53:39 -0800

    point home path to About page; remove unnecessary text on home page

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 		<Router>
 			<Navbar />
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="/" element={<About />} />
 				<Route path="/about" element={<About />} />
 				<Route
 					path="/blog"
