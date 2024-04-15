f661ce99ae895416cdc70fcd4192a6b0555e7713
Yiyu Chen <yiyuchen@Yiyus-MacBook-Pro.local>
2024-04-14 19:10:44 -0700

    Updates

---
+++ precache-manifest.79caf5a141b7ec5d410aac321d9ad2dd.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aac39a07517a0412fa403a7dcc95f90e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6295360df9af416936c6",
+    "url": "/static/css/main.8c51f4b0.chunk.css"
+  },
+  {
+    "revision": "88c65b319ac51159d681",
+    "url": "/static/js/2.dd4a2508.chunk.js"
+  },
+  {
+    "revision": "a167b981aa092d2ac722bf7946bd926d",
+    "url": "/static/js/2.dd4a2508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6295360df9af416936c6",
+    "url": "/static/js/main.ec66b70b.chunk.js"
+  },
+  {
+    "revision": "023c9830eee246aeae93",
+    "url": "/static/js/runtime-main.ce745d58.js"
+  },
+  {
+    "revision": "a84a1a24f005eeaf1a438ff5cdf78290",
+    "url": "/static/media/BAICD_BannerImage.a84a1a24.png"
+  },
+  {
+    "revision": "17b3a03dc8aa57ef690462b57bc880a4",
+    "url": "/static/media/alexwong_photo.17b3a03d.png"
+  },
+  {
+    "revision": "e24e46ddba363efe62695ec2a82fc034",
+    "url": "/static/media/arielliu_photo.e24e46dd.png"
+  },
+  {
+    "revision": "5aa3050c86fb01c415a111646e8577d0",
+    "url": "/static/media/elliewong_photo.5aa3050c.png"
+  },
+  {
+    "revision": "3feeef8dbebd3b0b42a288a597bdee3d",
+    "url": "/static/media/erinpang_photo.3feeef8d.png"
+  },
+  {
+    "revision": "28685cca748d33609be61e44b66269e4",
+    "url": "/static/media/genevievehodge_photo.28685cca.png"
+  },
+  {
+    "revision": "d93d5001636eb639a23e30a6ea09e62e",
+    "url": "/static/media/jimmywing_photo.d93d5001.png"
+  },
+  {
+    "revision": "47e45f8be1ff6c91a62d912caec097d8",
+    "url": "/static/media/laurauyeki_photo.47e45f8b.png"
+  },
+  {
+    "revision": "1305930c0cffb870143cb102c1805527",
+    "url": "/static/media/nalachen_photo.1305930c.png"
+  },
+  {
+    "revision": "6d0543639aee26acedfa449f099f3e32",
+    "url": "/static/media/nathanielgoh_photo.6d054363.png"
+  },
+  {
+    "revision": "1b867be64d28df925ebf086b8635d932",
+    "url": "/static/media/rebschan_photo.1b867be6.png"
+  },
+  {
+    "revision": "45de531995fc37db16469738e8c5fe68",
+    "url": "/static/media/shawnIntro_photo.45de5319.png"
+  },
+  {
+    "revision": "9d45946edb177c81e8d5cae5ca2c21d4",
+    "url": "/static/media/shirleycheng_photo.9d45946e.png"
+  },
+  {
+    "revision": "2d49eb7792f8424b29af03c4761f6efd",
+    "url": "/static/media/stefaniechen_photo.2d49eb77.png"
+  },
+  {
+    "revision": "0a5ca6778772cd60caeaa29ecdfe4a55",
+    "url": "/static/media/swathinarayanan_photo.0a5ca677.png"
+  },
+  {
+    "revision": "4df274a5b59a02d89a5c76107a5bb3ca",
+    "url": "/static/media/tiffanylin_photo.4df274a5.png"
+  }
+]);
